62428084c2c45fdd4b0145e6749467c44d9ff5f5
Nicholas Ali <111253105+NicholasAli1@users.noreply.github.com>
2026-02-23 18:26:09 -0400

    Add basic Redis commands and robust RESP parser
    Introduce new Redis API declarations (flushAll, set, get, keys, type, del, expire, rename) and an expiry_map in RedisDatabase.h to support TTLs. Improve parseRespCommand in RedisCommandHandler.cpp with strict RESP validation (CRLF/length checks, safe stoi, token reservation) and update processCommand to handle PING, ECHO, FLUSHALL, SET, GET, KEYS, TYPE, DEL/UNLINK, EXPIRE and RENAME with proper RESP responses and error messages. Also includes minor cleanup of includes and formatting.

--- include/RedisDatabase.h
+++ include/RedisDatabase.h
@@ -3,11 +3,30 @@
 #include <mutex>
 #include <unordered_map>
 #include <vector>
+#include <chrono>
 
 class RedisDatabase {
 public:
     // Get Singleton instance
     static RedisDatabase& getInstance();
+
+    // Common Commands
+    bool flushAll();
+
+    // Key/Value Ops
+    void set(const std::string& key, const std::string& value);
+    bool get(const std::string& key, const std::string& value);
+    std::vector<std::string> keys;
+    std::string type(const std::string& key);
+    bool del(const std::string& key);
+    bool expire(const std::string& key, const std::string& seconds);
+    bool rename(const std::string& oldKey, const std::string& newKey);
+    // TODO: RENAME
+
+    // TODO: LIST OPS
+
+    // TODO: HASH OPS
+
 
     // Persistence: Dump / Load the database from file.
     bool dump(const std::string& filename);
@@ -24,5 +43,9 @@ private:
     std::unordered_map<std::string, std::string> kv_store;
     std::unordered_map<std::string, std::vector<std::string>> list_store;
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> hash_store;
+
+    std::unordered_map<std::string, std::chrono::steady_clock::time_point> expiry_map;
+
+
 
 };
--- src/RedisCommandHandler.cpp
+++ src/RedisCommandHandler.cpp
@@ -1,7 +1,6 @@
 #include "../include/RedisCommandHandler.h"
 #include "../include/RedisDatabase.h"
 
-
 #include <vector>
 #include <sstream>
 #include <algorithm>
@@ -33,9 +32,18 @@ std::vector<std::string> parseRespCommand(const std::string &input) {
 
     // crlf = Carriage Return (\r), Line Feed (\n)
     size_t crlf = input.find("\r\n", pos);
-    if (crlf == std::string::npos) return tokens;
+    if (crlf == std::string::npos) return {};
 
-    int numElements = std::stoi(input.substr(pos, crlf - pos));
+    int numElements;
+    try {
+        numElements = std::stoi(input.substr(pos, crlf - pos));
+    } catch (...) {
+        return {};
+    }
+
+    if (numElements <= 0) return {};
+
+    tokens.reserve(numElements);
     pos = crlf + 2;
 
     for (int i = 0; i < numElements; ++i) {
@@ -43,15 +51,30 @@ std::vector<std::string> parseRespCommand(const std::string &input) {
         pos++; // Slip '$'
 
         crlf = input.find("\r\n", pos);
-        if (crlf == std::string::npos) break;
-        int len = std::stoi(input.substr(pos, crlf - pos));
+        if (crlf == std::string::npos) return {};
+
+        int len;
+        try {
+            len = std::stoi(input.substr(pos, crlf - pos));
+        } catch (...) {
+            return {};
+        }
+
+        if (len < 0) return {};
+
         pos = crlf + 2;
 
-        if (pos + len > input.size()) break;
+        if (pos + len + 2 > input.size()) return {};
+
         std::string token = input.substr(pos, len);
         tokens.push_back(token);
-        pos += len + 2;  // Skip Token and CRLF
+        pos += len;  // Skip Token
+
+        // Validate trailing CRLF
+        if (input.substr(pos, 2) != "\r\n") return {};
+        pos += 2;  // Skip CRLF
     }
+
     return tokens;
 }
 
@@ -65,7 +88,7 @@ RedisCommandHandler::RedisCommandHandler() {
 std::string RedisCommandHandler::processCommand(const std::string &commandLine) {
     // Use RESP parser
     auto tokens = parseRespCommand(commandLine);
-    if (tokens.empty()) return "-Error: Empty command\r\n";
+    if (tokens.empty()) return "-ERR Protocol error\r\n";
 
     std::string cmd = tokens[0];
     std::transform(cmd.begin(), cmd.end(), cmd.begin(), ::toupper);
@@ -73,20 +96,89 @@ std::string RedisCommandHandler::processCommand(const std::string &commandLine) 
     RedisDatabase& db = RedisDatabase::getInstance();
 
     // Check commands
+    // Common Commands
     if (cmd == "PING") {
         response << "+PONG\r\n";
     }
     else if (cmd == "ECHO") {
-        //...
+        if (tokens.size() < 2)
+            response << "-ERR ECHO requires input\r\n";
+        else
+            response << "$" << tokens[1].size() << "\r\n" << tokens[1] << "\r\n";
+    }
+    else if (cmd == "FLUSHALL") {   // Fixed typo
+        db.flushAll();
+        response << "+OK\r\n";
     }
     // TODO: Key/Value Ops
+    else if (cmd == "SET") {
+        if (tokens.size() < 3) {
+            response << "-ERR SET requires key AND value\r\n";
+        }
+        else {
+            db.set(tokens[1], tokens[2]);
+            response << "+OK\r\n";
+        }
+    }
+    else if (cmd == "GET") {
+        if (tokens.size() < 2) {
+            response << "-ERR GET requires key\r\n";
+        }
+        else {
+            std::string value;
+            if (db.get(tokens[1], value))
+                response << "$" << value.size() << "\r\n" << value << "\r\n";
+            else
+                response << "$-1\r\n";
+        }
+    }
+    else if (cmd == "KEYS") {
+        std::vector<std::string> allKeys = db.keys();
+        response << "*" << allKeys.size() << "\r\n";
+        for (const auto& key : allKeys) {
+            response << "$" << key.size() << "\r\n" << key << "\r\n";
+        }
+    }
+    else if (cmd == "TYPE") {
+        if (tokens.size() < 2)
+            response << "-ERR TYPE requires key\r\n";
+        else
+            response << "+" << db.type(tokens[1]) << "\r\n";
+    }
+    else if (cmd == "DEL" || cmd == "UNLINK") {
+        if (tokens.size() < 2)
+            response << "-ERR DEL requires key\r\n";
+        else {
+            int count = 0;
+            for (size_t i = 1; i < tokens.size(); ++i)
+                if (db.del(tokens[i]))
+                    count++;
+            response << ":" << count << "\r\n";
+        }
+    }
+    else if (cmd == "EXPIRE") {
+        if (tokens.size() < 3)
+            response << "-ERR EXPIRE requires key and time in seconds\r\n";
+        else {
+            bool result = db.expire(tokens[1], tokens[2]);
+            response << ":" << (result ? 1 : 0) << "\r\n";
+        }
+    }
+    else if (cmd == "RENAME") {
+        if (tokens.size() < 3)
+            response << "-ERR RENAME requires old key and new key names\r\n";
+        else {
+            bool result = db.rename(tokens[1], tokens[2]);
+            if (!result)
+                response << "-ERR no such key\r\n";
+            else
+                response << "+OK\r\n";
+        }
+    }
     // TODO: List Ops
     // TODO: Hash Ops
     else {
-        response << "-Error: Unknown command\r\n";
+        response << "-ERR Unknown command\r\n";
     }
-
-
     return response.str();
-
 }
